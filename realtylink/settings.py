cbe1498f1d006e229715211d7fd8723d967d27b4
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-01-18 15:49:00 +0200

    Updated houses spyder & added user-agent in settings.

--- realtylink/settings.py
+++ realtylink/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "realtylink.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "realtylink (+http://www.yourdomain.com)"
+USER_AGENT = "realtylink (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- realtylink/spiders/houses.py
+++ realtylink/spiders/houses.py
@@ -1,19 +1,21 @@
 import re
 import time
+from datetime import datetime
 
 import scrapy
 from scrapy.http import Response
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class HousesSpider(scrapy.Spider):
     name = "houses"
     allowed_domains = ["realtylink.org"]
     start_urls = ["https://realtylink.org/en/properties~for-rent"]
+    urls = []
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -27,10 +29,12 @@ class HousesSpider(scrapy.Spider):
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
         )
         self.driver = webdriver.Chrome(options=chrome_options)
+        self.start_time = datetime.now()
 
     def parse(self, response: Response, **kwargs):
         final_urls = set()
         self.driver.get(response.url)
+        time.sleep(1)
 
         while True:
             urls = self.driver.find_elements(By.CLASS_NAME, "a-more-detail")
@@ -44,6 +48,9 @@ class HousesSpider(scrapy.Spider):
                 break
             next_button.click()
             time.sleep(1)
+
+        for url in final_urls:
+            yield scrapy.Request(url, callback=self.parse_detail)
 
     def parse_detail(self, response):
         self.driver.get(response.url)
@@ -91,4 +98,8 @@ class HousesSpider(scrapy.Spider):
         }
 
     def closed(self, reason):
+        end_time = datetime.now()
+        elapsed_time = end_time - self.start_time
+        print(f"Script execution time: {elapsed_time}")
+
         self.driver.quit()
